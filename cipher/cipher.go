8512e46e22b3c10acebf1907b3ab177067ef412a
aryner <aryner@gmail.com>
2016-05-01 11:57:45 -0700

    added map of letter frequencies and pulled the printing of decoded maps to its own function

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -6,6 +6,14 @@ import (
 )
 
 var LettersNumbers []byte
+var LetterFrequency = map[byte]float64 {
+	'E':12.02, 'T':9.1, 'A':8.12, 'O':7.68, 'I':7.31,
+	'N':6.95, 'S':6.28, 'R':6.02, 'H':5.92, 'D':4.32,
+	'L':3.98, 'U':2.88, 'C':2.71, 'M':2.61, 'F':2.3,
+	'Y':2.11, 'W':2.09, 'G':2.03, 'P':1.82, 'B':1.49,
+	'V':1.11, 'K':0.69, 'X':0.17, 'Q':0.11, 'J':0.1,
+	'Z':0.07,
+}
 
 func initLetters() {
 	if len(LettersNumbers) == 0 {
@@ -35,14 +43,11 @@ func SingleByteXORDecode(coded []byte) {
 	for _, c := range LettersNumbers {
 		decode := SingleByteXORCipher(c,coded)
 		m[c] = decode
-		/*
-		fmt.Println(fmt.Sprintf("%c",byte(i)))
-		for _, v := range decode {
-			fmt.Printf("%c",v)
-		}
-		fmt.Println()
-		*/
 	}
+	printDecodedMaps(m)
+}
+
+func printDecodedMaps(m map[byte][]byte) {
 	for k, v := range m {
 		fmt.Println(fmt.Sprintf("%c",k))
 		for _, c := range v {
