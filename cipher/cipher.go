d1034787715d211671d012452af53d54c26e4854
aryner <aryner@gmail.com>
2016-05-01 12:11:41 -0700

    added lower case letter to the frequency map

--- cipher/cipher.go
+++ cipher/cipher.go
@@ -13,6 +13,12 @@ var LetterFrequency = map[byte]float64 {
 	'Y':2.11, 'W':2.09, 'G':2.03, 'P':1.82, 'B':1.49,
 	'V':1.11, 'K':0.69, 'X':0.17, 'Q':0.11, 'J':0.1,
 	'Z':0.07,
+	'e':12.02, 't':9.1, 'a':8.12, 'o':7.68, 'i':7.31,
+	'n':6.95, 's':6.28, 'r':6.02, 'h':5.92, 'd':4.32,
+	'l':3.98, 'u':2.88, 'c':2.71, 'm':2.61, 'f':2.3,
+	'y':2.11, 'w':2.09, 'g':2.03, 'p':1.82, 'b':1.49,
+	'v':1.11, 'k':0.69, 'x':0.17, 'q':0.11, 'j':0.1,
+	'z':0.07,
 }
 
 func initLetters() {
@@ -44,7 +50,21 @@ func SingleByteXORDecode(coded []byte) {
 		decode := SingleByteXORCipher(c,coded)
 		m[c] = decode
 	}
-	printDecodedMaps(m)
+	fmt.Println(ScoreAsEnglish(m['X']))
+	fmt.Println(ScoreAsEnglish(m['A']))
+}
+
+func ScoreMaps(m map[byte][]byte) {
+	return
+}
+
+func ScoreAsEnglish(b []byte) float64{
+	var result float64
+	for _, c := range b {
+		result += LetterFrequency[c]
+	}
+	result /= float64(len(b))
+	return result 
 }
 
 func printDecodedMaps(m map[byte][]byte) {
