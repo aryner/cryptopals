8879fbc5646ce3e00fa97ced3a88278a69aa0ec9
aryner <aryner@gmail.com>
2016-05-18 12:33:10 -0700

    added aes rotate

--- aes/aes.go
+++ aes/aes.go
@@ -6,6 +6,7 @@ import (
 
 var reducingPoly = int(283)
 
+//This example has cache, timing, and branch prediction side-channel leaks, and is not suitable for use in cryptography.
 func peasantsMult(a int, b int) int {
 	var p = int(0)
 	for b > 0 {
@@ -21,6 +22,14 @@ func peasantsMult(a int, b int) int {
 	return p
 }
 
+//Rotates a 32 bit word 8 bits to the left, wrapping back to the begining
+func Rotate(word int) int {
+	tail := (word & 0xff000000) >> 24
+	rotated := ((word << 8) & 0xffffff00) ^ tail
+	return rotated
+}
+
 func main() {
 	fmt.Printf("%b\n",peasantsMult(83,202))
+	fmt.Printf("%x\n",Rotate(0x1d2c3a4f))
 }
